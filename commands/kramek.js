df9b640b0cf10ed3cfc12edbf5a72a147e381689
Jakub-ModPol <62879844+Jakub-ModPol@users.noreply.github.com>
2020-04-07 21:13:14 +0200

    Update kramek.js

--- commands/kramek.js
+++ commands/kramek.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-    let file = fs.readFileSync(".\\kramek.txt", {encoding: "utf-8"})
+    let file = fs.readFileSync(".//kramek.txt", {encoding: "utf-8"})
     let answers = file.split('\n')
     let randomAnswer = answers[Math.floor(Math.random() * (answers.length - 1))]
     message.channel.send(randomAnswer)

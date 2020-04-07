ea391210aa07629241212d20b3a4a0333d4e0a58
Jakub-ModPol <62879844+Jakub-ModPol@users.noreply.github.com>
2020-04-07 16:21:17 +0200

    Update zrcadleni.js
    Locked

--- commands/zrcadleni.js
+++ commands/zrcadleni.js
@@ -2,10 +2,14 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
+
+    const logChannel = message.guild.channels.find(c => c.name === "🏰zrcadlovka");
+    if(message.channel != logChannel) return message.member.send("**Tento příkaz můžeš použít jen v kanálu** <#678707102646272040>");
+
     let file = fs.readFileSync(".//zrcadlo.txt", {encoding: "utf-8"})
     let answers = file.split('\n')
     let randomAnswer = answers[Math.floor(Math.random() * (answers.length - 1))]
-    message.channel.send(randomAnswer)
+    logChannel.send(randomAnswer)
 }
 module.exports.help = {
     name: "zrcadleni" 

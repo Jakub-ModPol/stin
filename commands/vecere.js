e95c438232cbad931d3790d2e90d2840e25cce0b
Jakub-ModPol <62879844+Jakub-ModPol@users.noreply.github.com>
2020-04-07 16:20:15 +0200

    Update vecere.js
    Locked

--- commands/vecere.js
+++ commands/vecere.js
@@ -2,10 +2,14 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
+
+    const logChannel = message.guild.channels.find(c => c.name === "🏰jídelna");
+    if(message.channel != logChannel) return message.member.send("**Tento příkaz můžeš použít jen v kanálu** <#677945410051899412>");
+
     let file = fs.readFileSync(".//vecere.txt", {encoding: "utf-8"})
     let answers = file.split('\n')
     let randomAnswer = answers[Math.floor(Math.random() * (answers.length - 1))]
-    message.channel.send(randomAnswer)
+    logChannel.send(randomAnswer)
 }
 module.exports.help = {
     name: "vecere" 

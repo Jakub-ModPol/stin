d3b34c9a990d48eb0b2009996c47257080a29bb8
Jakub-ModPol <62879844+Jakub-ModPol@users.noreply.github.com>
2020-04-07 16:19:43 +0200

    Update stinovani.js
    Locked

--- commands/stinovani.js
+++ commands/stinovani.js
@@ -2,10 +2,14 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
+
+    const logChannel = message.guild.channels.find(c => c.name === "🏰vstupní-hala");
+    if(message.channel != logChannel) return message.member.send("**Tento příkaz můžeš použít jen v kanálu** <#678659022030635022>");
+
     let file = fs.readFileSync(".//stinovani.txt", {encoding: "utf-8"})
     let answers = file.split('\n')
     let randomAnswer = answers[Math.floor(Math.random() * (answers.length - 1))]
-    message.channel.send(randomAnswer)
+    logChannel.send(randomAnswer)
 }
 module.exports.help = {
     name: "stinovani" 

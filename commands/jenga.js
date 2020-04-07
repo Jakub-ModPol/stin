842d93977efe218f3123d77447d1e56125f141b7
Jakub-ModPol <62879844+Jakub-ModPol@users.noreply.github.com>
2020-04-07 21:09:32 +0200

    Update jenga.js

--- commands/jenga.js
+++ commands/jenga.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
     
-    let file = fs.readFileSync(".\\jenga.txt", {encoding: "utf-8"})
+    let file = fs.readFileSync(".//jenga.txt", {encoding: "utf-8"})
     let answers = file.split('\n')
     let randomAnswer = answers[Math.floor(Math.random() * (answers.length - 1))]
 
